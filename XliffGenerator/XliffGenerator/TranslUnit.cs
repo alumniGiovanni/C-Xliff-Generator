65c4c20edebe7072105ab5abbde4069f0ce223eb
Giovanni Andrade <giovanni.andrade9991@gmail.com>
2020-11-12 18:48:21 -0300

    idk fr

--- XliffGenerator/XliffGenerator/TranslUnit.cs
+++ XliffGenerator/XliffGenerator/TranslUnit.cs
@@ -2,9 +2,17 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace XliffGenerator
+namespace TranslatorConsole
 {
-    class TranslUnit
+    public class TranslUnit
     {
+        public string Source { get; private set; }
+        public string Target { get; private set; }
+
+        public TranslUnit(string source, string target)
+        {
+            Source = source;
+            Target = target;
+        }
     }
 }
