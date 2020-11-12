3c0d2ee2a1d81ee2b2ffd027bb772b0b96445be5
Giovanni Andrade <giovanni.andrade9991@gmail.com>
2020-11-12 18:31:04 -0300

    Initial

---
+++ XliffGenerator/XliffGenerator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace XliffGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
